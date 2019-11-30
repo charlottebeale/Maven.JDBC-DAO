35ecc380f5422f08738f078a6889779aa071ec00
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2019-11-29 20:23:34 -0500

    Started

---
+++ src/main/java/daos/Car.java
@@ -0,0 +1,67 @@
+package daos;
+
+public class Car {
+    private int id;
+    private String make;
+    private String model;
+    private String year;
+    private String color;
+    private String vin;
+
+    public Car(int id, String make, String model, String year, String color, String vin) {
+        this.id = id;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.vin = vin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getVin() {
+        return vin;
+    }
+
+    public void setVin(String vin) {
+        this.vin = vin;
+    }
+}
